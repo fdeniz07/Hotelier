7614fa60a52afcb3b1c93160aef3c285c315971f
Fatih Deniz <deniz.fatih@hotmail.de>
2023-04-06 19:31:32 +0200

    BLL Revision

---
+++ src/Backend/HotelProject.BusinessLayer/Extensions/ServiceRegistraion.cs
@@ -0,0 +1,19 @@
+﻿namespace HotelProject.BusinessLayer.Extensions
+{
+    using Abstracts;
+    using Concretes;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ServiceRegistraion
+    {
+        public static void AddBusinessLayerServices(this IServiceCollection services)
+        {
+            //IOC Container'e eklenmesi icin BusinessLayer katmanindaki abstract ve concrete objeleri geciyoruz
+            services.AddScoped<IRoomService, RoomManager>();
+            services.AddScoped<IServiceService, ServiceManager>();
+            services.AddScoped<IStaffService, StaffManager>();
+            services.AddScoped<ISubscribeService, SubscribeManager>();
+            services.AddScoped<ITestimonialService, TestimonialManager>();
+        }
+    }
+}
