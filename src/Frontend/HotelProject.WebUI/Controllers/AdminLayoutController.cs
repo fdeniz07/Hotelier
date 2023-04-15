c789e45e4f548cd586aad16d6e17a042fe8c75bb
Fatih Deniz <deniz.fatih@hotmail.de>
2023-04-15 06:33:57 +0200

    WebUI process 1

--- src/Frontend/HotelProject.WebUI/Controllers/AdminLayoutController.cs
+++ src/Frontend/HotelProject.WebUI/Controllers/AdminLayoutController.cs
@@ -8,5 +8,46 @@ namespace HotelProject.WebUI.Controllers
         {
             return View();
         }
+
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+
+
+        public PartialViewResult PreLoaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult NavHeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult SideBarPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult BreadCrumbPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult ScriptPartial()
+        {
+            return PartialView();
+        }
     }
 }
