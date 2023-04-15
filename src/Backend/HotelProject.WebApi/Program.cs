0bf1434e4afc6e481e6434edc083f7b41119792f
Fatih Deniz <deniz.fatih@hotmail.de>
2023-04-15 06:34:14 +0200

    Adding CORS Policies

--- src/Backend/HotelProject.WebApi/Program.cs
+++ src/Backend/HotelProject.WebApi/Program.cs
@@ -6,9 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 //builder.Services.AddDbContext<MsDbContext>(); -->Bunu artik AddDataAccessLayerService icerisinde geciyoruz
- 
+
 builder.Services.AddDataAccessLayerServices();
 builder.Services.AddBusinessLayerServices();
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("OtelApiCors", opts =>
+    {
+        opts.AllowAnyOrigin().
+            AllowAnyHeader().
+            AllowAnyMethod();
+    });
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -25,7 +36,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("OtelApiCors");
 app.UseAuthorization();
 
 app.MapControllers();
