4f8af73733f43e6bab5e90d5b9fd6f6e36f30913
Fatih Deniz <deniz.fatih@hotmail.de>
2023-04-20 23:59:28 +0200

    AutoMapper

---
+++ src/Frontend/HotelProject.WebUI/Mapping/AutoMapper/Profile/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelProject.WebUI.Mapping.AutoMapper.Profile
+{
+    using EntityLayer.Concretes;
+    using EntityLayer.Dtos.RoomDto;
+    using global::AutoMapper;
+
+    public class UserProfile:Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<RoomAddDto, Room>();
+        }
+    }
+}
